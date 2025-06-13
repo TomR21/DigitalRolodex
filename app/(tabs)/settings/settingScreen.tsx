01d651a0ce0b5c34e00a07de2a7538f1559c2378
Tom van Rees <tomvanrees@gmail.com>
2025-06-13 19:27:13 +0200

    Added share database button

--- app/(tabs)/settings/settingScreen.tsx
+++ app/(tabs)/settings/settingScreen.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
 
 import { Styles } from '@/constants/Styles';
+
+
+async function shareDB() {
+  // Finds the location of the SQL database and upons a FileSharing window
+  await Sharing.shareAsync(
+    FileSystem.documentDirectory + 'SQLite/contactData', 
+    {dialogTitle: 'share or copy your DB via'}
+  ).catch(error =>{
+   console.log(error);
+})
+}
 
 
 function settingScreen() {
   return (
     <View>
       <Text style={Styles.text}> Setting 1 </Text>
+      <Button title="Share DB" onPress={() => shareDB()}/>  
     </View>
   );
 }
